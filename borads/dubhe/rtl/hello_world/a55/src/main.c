f6902a2b29da3814da8d85fb18f8da31a508d0ab
sk <1193375474@qq.com>
2022-02-16 11:29:11 +0800

    [linkerscript] - rom - 64KB, ram - 128KB

--- borads/dubhe/rtl/hello_world/a55/src/main.c
+++ borads/dubhe/rtl/hello_world/a55/src/main.c
@@ -6,10 +6,10 @@
 #define REG16(addr) (*(volatile uint16_t *)(uint32_t)(addr))
 #define REG8(addr) (*(volatile uint8_t *)(uint32_t)(addr))
 
-#define CORE0_ADDR (0x00a40000 + 0)
-#define CORE1_ADDR (0x00a40000 + 8)
-#define CORE2_ADDR (0x00a40000 + 16)
-#define CORE3_ADDR (0x00a40000 + 24)
+#define CORE0_ADDR (0x00a10000 + 0)
+#define CORE1_ADDR (0x00a10000 + 8)
+#define CORE2_ADDR (0x00a10000 + 16)
+#define CORE3_ADDR (0x00a10000 + 24)
 
 int main()
 {
