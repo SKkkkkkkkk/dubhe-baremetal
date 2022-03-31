ab5822aa8130e5777ac20aa9b9c609497fd7a68d
sk <1193375474@qq.com>
2022-03-31 10:32:08 +0800

    PROJECT_PATH使用当前命令行的相对路径

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-cmake -S . -B build/cv32e40p -DDEVICE=CV32E40P -DPROJECT_NAME=hello -DPROJECT_PATH=app/hello_world
-cmake --build build/cv32e40p -j100 
+cmake -S . -B build/a55 -DDEVICE=A55 -DPROJECT_NAME=hello -DPROJECT_PATH=app/hello_world
+cmake --build build/A55 -j100 
