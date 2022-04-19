824b76478c06a67bf8deeaf8398213efb0175554
sk <1193375474@qq.com>
2022-04-19 23:44:57 +0800

    xlat_tables_v2 qemu 测试通过

--- app/hello_world/inc/main_a55.c
+++ app/hello_world/inc/main_a55.c
@@ -4,6 +4,7 @@
 #include "pl001.h"
 #include "gic.h"
 #include "irq_ctrl.h"
+#include "xlat_tables_v2.h"
 
 uint32_t get_daif()
 {
@@ -86,10 +87,45 @@ void irq32_handler(void)
 	printf("\tin irq d0:%f\n", d0);
 }
 
+#define MAP_ROM MAP_REGION_FLAT( \
+0x00000000, \
+0x04000000, \
+MT_MEMORY|MT_RO|MT_SECURE )
+
+#define MAP_RAM MAP_REGION_FLAT( \
+0x0e000000, \
+0x01000000, \
+MT_MEMORY|MT_RW|MT_SECURE )
+
+
+#define DEVICE0_BASE			0x08000000
+#define DEVICE0_SIZE			0x01000000
+#define DEVICE1_BASE			0x09000000
+#define DEVICE1_SIZE			0x00c00000
+
+#define MAP_DEVICE0	MAP_REGION_FLAT(DEVICE0_BASE,			\
+					DEVICE0_SIZE,			\
+					MT_DEVICE | MT_RW | MT_SECURE)
+
+#define MAP_DEVICE1	MAP_REGION_FLAT(DEVICE1_BASE,			\
+					DEVICE1_SIZE,			\
+					MT_DEVICE | MT_RW | MT_SECURE)
+
 int main()
 {
 	printf("hello world,in core.%u\n", get_core_id());
 
+	const mmap_region_t plat_bl1_region[] = {
+		MAP_ROM,
+		MAP_RAM,
+		MAP_DEVICE0,
+		MAP_DEVICE1,
+		{0}
+	};
+	(void)plat_bl1_region;
+	mmap_add(plat_bl1_region);
+	init_xlat_tables();
+	enable_mmu_el3(0);
 	IRQ_Initialize();
 	IRQ_SetHandler(32, irq32_handler);
 	IRQ_SetPriority(32, 0);
