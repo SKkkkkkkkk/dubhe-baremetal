945c2eced7c82ebc266dab7e5f55442d781694a6
sk <astronautsk@qq.com>
2022-09-14 20:38:52 +0800

    uart驱动换会saturn版本。 16550驱动未配置DLF寄存器，存在精确度不足问题。

--- app/a55/bootrom_for_fpga_bit/build.sh
+++ app/a55/bootrom_for_fpga_bit/build.sh
@@ -1,2 +1,2 @@
-cmake -S ../../../ -B build -DPROJECT_NAME=bootrom_for_fpga_bit -DPROJECT_PATH=. -DCORE=A55 -DBOARD=RTL -DBUILD_TYPE=Debug -DWHERE_TO_LOAD=ROM_AND_RAM
+cmake -S ../../../ -B build -DPROJECT_NAME=bootrom_for_fpga_bit -DPROJECT_PATH=. -DCORE=A55 -DBOARD=FPGA -DBUILD_TYPE=Debug -DWHERE_TO_LOAD=ROM_AND_RAM
 cmake --build build -j100
