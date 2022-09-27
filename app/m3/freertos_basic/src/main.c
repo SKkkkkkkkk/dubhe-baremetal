0f9bc0dd00b9c144f18f77a1007df3d9f74ef676
sk <astronautsk@qq.com>
2022-09-27 11:05:37 +0800

    nothing matters.

--- app/m3/freertos_basic/src/main.c
+++ app/m3/freertos_basic/src/main.c
@@ -6,9 +6,9 @@
 int main()
 {
 	// printf("hello world.\n\r");
-	SCB->SHCSR |= 7<<16;
-	printf("SCB->SHCSR: 0x%lx\n\r", SCB->SHCSR);
-	__DSB();
+	// SCB->SHCSR |= 7<<16;
+	// printf("SCB->SHCSR: 0x%lx\n\r", SCB->SHCSR);
+	// __DSB();
 	void task1(void* arg);
 	void task2(void* arg);
 	if (xTaskCreate(task1, "task1", configMINIMAL_STACK_SIZE, NULL, 1, NULL) != pdPASS)
