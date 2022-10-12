92c5687a53066d1086a650b0d2a000adb624d031
lizhenhui <lizhenhui@seehi.com>
2022-10-12 14:20:48 +0800

    [pm] wfi sleep wakeup ok
    Signed-off-by: lizhenhui <lizhenhui@seehi.com>

--- app/m3/pm_demo/src/freertos_test.c
+++ app/m3/pm_demo/src/freertos_test.c
@@ -2,79 +2,76 @@
 #include "task.h"
 #include "semphr.h"
 
-#include <stdint.h>
 #include <stdbool.h>
+#include <stdint.h>
 #include <stdio.h>
 
 #include "pm.h"
 
-#define TEST_SLEEP          0
-#define TEST_STANDBY        1
-#define TEST_HIBERNATION    0
+#define TEST_SLEEP       0
+#define TEST_STANDBY     1
+#define TEST_HIBERNATION 0
 
 static SemaphoreHandle_t pmSemaphore;
-int32_t arg_flag = 1;
+int32_t                  arg_flag = 1;
 
-void task1(void* arg)
-{
-	pmSemaphore = xSemaphoreCreateBinary();
+void task1(void *arg) {
+    pmSemaphore = xSemaphoreCreateBinary();
 
-	while(1)
-	{
-		printf("task1\n");
-		if (xSemaphoreTake(pmSemaphore, portMAX_DELAY)) {
-			/* pm_set_test_level(TEST_CORE); */
-			printf("\nPM example start!\n");
-			printf("Support 3 low power modes: sleep/standby/hibernation\n");
+    while (1) {
+        printf("task1\n");
+        if (xSemaphoreTake(pmSemaphore, portMAX_DELAY)) {
+            /* pm_set_test_level(TEST_CORE); */
+            printf("\nPM example start!\n");
+            printf("Support 3 low power modes: sleep/standby/hibernation\n");
 
 #if TEST_SLEEP
-			/*enetr sleep test*/
-			printf("Enter sleep mode, setup wakeup source irq&timer&button\n");
-			/* 唤醒源配置初始化 */
+            /*enetr sleep test*/
+            printf("Enter sleep mode, setup wakeup source irq&timer&button\n");
+            /* 唤醒源配置初始化 */
 
-			pm_enter_mode(PM_MODE_SLEEP);
-			printf("Exit sleep mode\n\n");
-			/* 唤醒源配置关闭 */
+            pm_enter_mode(PM_MODE_SLEEP);
+            printf("Exit sleep mode\n\n");
+            /* 唤醒源配置关闭 */
 #endif
 #if TEST_STANDBY
-			/*enetr sleep test*/
-			printf("Enter standby mode, setup wakeup source irq&timer&button\n");
-			/* 唤醒源配置初始化 */
+            /*enetr sleep test*/
+            printf(
+                "Enter standby mode, setup wakeup source irq&timer&button\n");
+            /* 唤醒源配置初始化 */
 
-			pm_enter_mode(PM_MODE_STANDBY);
-			printf("Exit standby mode\n\n");
-			/* 唤醒源配置关闭 */
+            pm_enter_mode(PM_MODE_STANDBY);
+            printf("Exit standby mode\n\n");
+            /* 唤醒源配置关闭 */
 #endif
 #if TEST_HIBERNATION
-			/*enetr sleep test*/
-			printf("Enter hibernation mode, setup wakeup source irq&timer&button\n");
-			/* 唤醒源配置初始化 */
+            /*enetr sleep test*/
+            printf("Enter hibernation mode, setup wakeup source "
+                   "irq&timer&button\n");
+            /* 唤醒源配置初始化 */
 
-			pm_enter_mode(PM_MODE_HIBERNATION);
-			printf("Exit hibernation mode\n\n");
-			/* 唤醒源配置关闭 */
+            pm_enter_mode(PM_MODE_HIBERNATION);
+            printf("Exit hibernation mode\n\n");
+            /* 唤醒源配置关闭 */
 #endif
-			// NVIC->ISPR[(((uint32_t)Interrupt0_IRQn) >> 5UL)] = (uint32_t)(1UL << (((uint32_t)Interrupt0_IRQn) & 0x1FUL));
-		}
-		vTaskDelay(100);
-	}
+            // NVIC->ISPR[(((uint32_t)Interrupt0_IRQn) >> 5UL)] = (uint32_t)(1UL
+            // << (((uint32_t)Interrupt0_IRQn) & 0x1FUL));
+        }
+        vTaskDelay(100);
+    }
 }
 
-
-void single_func(void)
-{
-		printf("task2\n");
-		vTaskDelay(1000);
-		/* if(test_count++ < 1) */
-			xSemaphoreGive(pmSemaphore);
-}
-void task2(void* arg)
-{
-	/* int32_t test_count = 0; */
-
-	while(1)
-	{
-		single_func();
-	}
+void single_func(void) {
+    printf("task2\n");
+    vTaskDelay(1000);
+    /* if(test_count++ < 1) */
+    xSemaphoreGive(pmSemaphore);
 }
 
+void task2(void *arg) {
+    /* int32_t test_count = 0; */
+
+    while (1) {
+        single_func();
+    }
+}
--- app/m3/pm_demo/src/main.c
+++ app/m3/pm_demo/src/main.c
@@ -1,234 +1,224 @@
-#include "m3.h"
+#include <stdbool.h>
 #include <stdio.h>
 #include <string.h>
-#include <stdbool.h>
+
 #include "FreeRTOS.h"
 #include "task.h"
-#include "pm.h"
-#include "mem_and_clock.h"
 #include "axp2101.h"
-#include "systimer.h"
 #include "dw_apb_gpio.h"
+#include "m3.h"
+#include "mem_and_clock.h"
+#include "pm.h"
+#include "systimer.h"
 
-#define TEST_SLEEP          0
-#define TEST_STANDBY        1
-#define TEST_HIBERNATION    0
-#define STACK_SIZE			1024
-#define Interrupt79_IRQn    79
-#define GPIO0				0x410A0000UL
+#define TEST_SLEEP           0
+#define TEST_STANDBY         1
+#define TEST_HIBERNATION     0
+#define STACK_SIZE           1024
+#define Interrupt79_IRQn     79
+#define GPIO0                0x410A0000UL
 
-#define KEY_EINT_PIN                   10
-#define KEY_EINT_PIN_GROUP             (KEY_EINT_PIN/32)
-#define KEY_EINT_PIN_NUM               (KEY_EINT_PIN%32)
+#define KEY_EINT_PIN         10
+#define KEY_EINT_PIN_GROUP   (KEY_EINT_PIN / 32)
+#define KEY_EINT_PIN_NUM     (KEY_EINT_PIN % 32)
 
-#define ROUND_UP(x, align) (((int) (x) + (align - 1)) & ~(align - 1))
-#define ROUND_DOWN(x, align) ((int)(x) & ~(align - 1))
+#define ROUND_UP(x, align)   (((int) (x) + (align - 1)) & ~(align - 1))
+#define ROUND_DOWN(x, align) ((int) (x) & ~(align - 1))
 
-extern const VECTOR_TABLE_Type __VECTOR_TABLE[240];
-__ramvector VECTOR_TABLE_Type __VECTOR_TABLE_IN_SRAM[240];
+extern const VECTOR_TABLE_Type __VECTOR_TABLE[ 240 ];
+__ramvector VECTOR_TABLE_Type  __VECTOR_TABLE_IN_SRAM[ 240 ];
 
-__ramdata int count = 0;
+__ramdata int          count = 0;
 __ramdata StaticTask_t xTaskBuffer;
-__ramdata StackType_t xStack[STACK_SIZE];
-__ramdata StackType_t IdleTaskStack[configMINIMAL_STACK_SIZE];
+__ramdata StackType_t  xStack[ STACK_SIZE ];
+__ramdata StackType_t  IdleTaskStack[ configMINIMAL_STACK_SIZE ];
 __ramdata StaticTask_t IdleTaskTCB;
-__ramdata StackType_t TimerTaskStack[configTIMER_TASK_STACK_DEPTH];
+__ramdata StackType_t  TimerTaskStack[ configTIMER_TASK_STACK_DEPTH ];
 __ramdata StaticTask_t TimerTaskTCB;
 
-
 void vApplicationGetIdleTaskMemory(StaticTask_t **ppxIdleTaskTCBBuffer,
-									StackType_t **ppxIdleTaskStackBuffer,
-									uint32_t *pulIdleTaskStackSize)
-{
-	*ppxIdleTaskTCBBuffer=&IdleTaskTCB;
-	*ppxIdleTaskStackBuffer=IdleTaskStack;
-	*pulIdleTaskStackSize=configMINIMAL_STACK_SIZE;
+                                   StackType_t  **ppxIdleTaskStackBuffer,
+                                   uint32_t      *pulIdleTaskStackSize) {
+    *ppxIdleTaskTCBBuffer   = &IdleTaskTCB;
+    *ppxIdleTaskStackBuffer = IdleTaskStack;
+    *pulIdleTaskStackSize   = configMINIMAL_STACK_SIZE;
 }
 
 void vApplicationGetTimerTaskMemory(StaticTask_t **ppxTimerTaskTCBBuffer,
-									StackType_t **ppxTimerTaskStackBuffer,
-									uint32_t *pulTimerTaskStackSize)
-{
-	*ppxTimerTaskTCBBuffer=&TimerTaskTCB;
-	*ppxTimerTaskStackBuffer=TimerTaskStack;
-	*pulTimerTaskStackSize=configTIMER_TASK_STACK_DEPTH;
+                                    StackType_t  **ppxTimerTaskStackBuffer,
+                                    uint32_t      *pulTimerTaskStackSize) {
+    *ppxTimerTaskTCBBuffer   = &TimerTaskTCB;
+    *ppxTimerTaskStackBuffer = TimerTaskStack;
+    *pulTimerTaskStackSize   = configTIMER_TASK_STACK_DEPTH;
 }
 
-__ramfunc void irq_handler0(void)
-{
-	count++;
-	// printf("hello world in irq 0. %d\n", count);
-	*(uint32_t *)(__EXTERNAL_RAM_BASE + 4) = count;
-	return;
+__ramfunc void irq_handler0(void) {
+    count++;
+    // printf("hello world in irq 0. %d\n", count);
+    *(uint32_t *) (__EXTERNAL_RAM_BASE + 4) = count;
+    return;
 }
 
-void irq_handler1(void)
-{
-	count++;
-	printf("hello world in irq 1. %d\n", count);
-	return;
+void irq_handler1(void) {
+    count++;
+    printf("hello world in irq 1. %d\n", count);
+    return;
 }
 
-void irq_handler_gpio(void)
-{
-	uint32_t intstatus = *((uint32_t *)(GPIO0 + 0x40));
+void irq_handler_gpio(void) {
+    uint32_t intstatus = *((uint32_t *) (GPIO0 + 0x40));
 
-	count++;
+    count++;
 
-	if(intstatus&(1<<KEY_EINT_PIN_NUM)){
-		gpio_clear_interrput(KEY_EINT_PIN_GROUP, KEY_EINT_PIN_NUM);
-		printf("gpio in irq 79. %d\n", count);
-	}else{
-		printf("irq is not 79\n");
-	}
+    if (intstatus & (1 << KEY_EINT_PIN_NUM)) {
+        gpio_clear_interrput(KEY_EINT_PIN_GROUP, KEY_EINT_PIN_NUM);
+        printf("gpio in irq 79. %d\n", count);
+    } else {
+        printf("irq is not 79\n");
+    }
 
-	return;
+    return;
 }
 
-void hardware_init_hook(void)
-{
-	/* 时钟相关 */
-	systimer_init();
-	pm_init();
-	/* 判断启动源 */
-	/* 中断相关 */
-	/* RTC相关 */
+void hardware_init_hook(void) {
+    /* 时钟相关 */
+    systimer_init();
+    pm_init();
+    /* 判断启动源 */
+    /* 中断相关 */
+    /* RTC相关 */
 }
 
-static int pm_suspend(struct soc_device *dev, enum suspend_state_t state)
-{
-	printf("--> %s.\n", __func__);
-	return 0;
+static int pm_suspend(struct soc_device *dev, enum suspend_state_t state) {
+    printf("--> %s.\n", __func__);
+    return 0;
 }
 
-static int pm_resume(struct soc_device *dev, enum suspend_state_t state)
-{
-	printf("--> %s.\n", __func__);
-	return 0;
+static int pm_resume(struct soc_device *dev, enum suspend_state_t state) {
+    printf("--> %s.\n", __func__);
+    return 0;
 }
 
 static const struct soc_device_driver pm_drv = {
-	.name = "pm_drv",
-	.suspend = pm_suspend,
-	.resume = pm_resume,
+    .name    = "pm_drv",
+    .suspend = pm_suspend,
+    .resume  = pm_resume,
 };
 
 static struct soc_device pm_dev = {
-	.name = "pm_dev",
-	.driver = &pm_drv,
-	.platform_data = NULL,
+    .name          = "pm_dev",
+    .driver        = &pm_drv,
+    .platform_data = NULL,
 };
 
 #define PM_DEV (&pm_dev)
 
-static int pm_suspend_noirq(struct soc_device *dev, enum suspend_state_t state)
-{
-	printf("--> %s.\n", __func__);
-	return 0;
+static int pm_suspend_noirq(struct soc_device   *dev,
+                            enum suspend_state_t state) {
+    printf("--> %s.\n", __func__);
+    return 0;
 }
 
-static int pm_resume_noirq(struct soc_device *dev, enum suspend_state_t state)
-{
-	printf("--> %s.\n", __func__);
-	return 0;
+static int pm_resume_noirq(struct soc_device *dev, enum suspend_state_t state) {
+    printf("--> %s.\n", __func__);
+    return 0;
 }
 
 static const struct soc_device_driver pm_noirq_drv = {
-	.name = "pm_noirq_drv",
-	.suspend_noirq = pm_suspend_noirq,
-	.resume_noirq = pm_resume_noirq,
+    .name          = "pm_noirq_drv",
+    .suspend_noirq = pm_suspend_noirq,
+    .resume_noirq  = pm_resume_noirq,
 };
 
 static struct soc_device pm_noirq_dev = {
-	.name = "pm_noirq_dev",
-	.driver = &pm_noirq_drv,
-	.platform_data = NULL,
+    .name          = "pm_noirq_dev",
+    .driver        = &pm_noirq_drv,
+    .platform_data = NULL,
 };
 
 #define PM_NOIRQ_DEV (&pm_noirq_dev)
 
-__ramfunc static inline void _mdelay(unsigned long msec)
-{
-#define udelay(x)                                                                                                      \
-	{                                                                                                              \
-		unsigned xx = x;                                                                                       \
-		while (xx--) {                                                                                         \
-			asm volatile("nop");                                                                           \
-		}                                                                                                      \
-	}
-	while (msec--)
-		udelay(500);
+__ramfunc static inline void _mdelay(unsigned long msec) {
+#define udelay(x)                \
+    {                            \
+        unsigned xx = x;         \
+        while (xx--) {           \
+            asm volatile("nop"); \
+        }                        \
+    }
+    while (msec--)
+        udelay(500);
 }
 
-__ramfunc void clear_ddr(void)
-{
+__ramfunc void clear_ddr(void) {
+    extern int __bss_end__[];
+    int        i    = 0;
+    int        size = (uint32_t) __bss_end__ - __EXTERNAL_RAM_BASE;
 
-	extern int __bss_end__[];
-	int i = 0;
-	int size = (uint32_t)__bss_end__ - __EXTERNAL_RAM_BASE;
+    printf("ddr __bss_end__ 0x%lx size 0x%x up 0x%x\n", (uint32_t) __bss_end__,
+           size, ROUND_UP(size, 128));
 
-	printf("ddr __bss_end__ 0x%lx size 0x%x up 0x%x\n", (uint32_t)__bss_end__, size, ROUND_UP(size, 128));
+    for (i = 0; i < ROUND_UP(size, 128); i += 4) {
+        *(uint32_t *) (__EXTERNAL_RAM_BASE + i) = 0;
+    }
 
-	for(i=0; i<ROUND_UP(size, 128); i+=4){
-		*(uint32_t *)(__EXTERNAL_RAM_BASE + i) = 0;
-	}
-
-	i = 0;
-	while(1){
-		_mdelay(500);
-		*(uint32_t *)(__EXTERNAL_RAM_BASE) = i++;
-		__DSB();
-		__ISB();
-		NVIC->ISPR[(((uint32_t)Interrupt0_IRQn) >> 5UL)] = (uint32_t)(1UL << (((uint32_t)Interrupt0_IRQn) & 0x1FUL));
-	}
+    i = 0;
+    while (1) {
+        _mdelay(500);
+        *(uint32_t *) (__EXTERNAL_RAM_BASE) = i++;
+        __DSB();
+        __ISB();
+        NVIC->ISPR[ (((uint32_t) Interrupt0_IRQn) >> 5UL) ] =
+            (uint32_t) (1UL << (((uint32_t) Interrupt0_IRQn) & 0x1FUL));
+    }
 }
 
-int main()
-{
-	memcpy(__VECTOR_TABLE_IN_SRAM, __VECTOR_TABLE, sizeof(__VECTOR_TABLE_IN_SRAM));
-	__DSB();
-	SCB->VTOR = (uint32_t) &(__VECTOR_TABLE_IN_SRAM[0]);
-	__DSB();
-	__ISB();
+int main() {
+    memcpy(__VECTOR_TABLE_IN_SRAM, __VECTOR_TABLE,
+           sizeof(__VECTOR_TABLE_IN_SRAM));
+    __DSB();
+    SCB->VTOR = (uint32_t) & (__VECTOR_TABLE_IN_SRAM[ 0 ]);
+    __DSB();
+    __ISB();
 
-	NVIC_SetVector(Interrupt0_IRQn, (uintptr_t)irq_handler0);
-	NVIC_EnableIRQ(Interrupt0_IRQn);
-	NVIC_SetPendingIRQ(Interrupt0_IRQn);
+    NVIC_SetVector(Interrupt0_IRQn, (uintptr_t) irq_handler0);
+    NVIC_EnableIRQ(Interrupt0_IRQn);
+    NVIC_SetPendingIRQ(Interrupt0_IRQn);
 
-	NVIC_SetVector(Interrupt1_IRQn, (uintptr_t)irq_handler1);
-	NVIC_EnableIRQ(Interrupt1_IRQn);
-	NVIC_SetPendingIRQ(Interrupt1_IRQn);
+    NVIC_SetVector(Interrupt1_IRQn, (uintptr_t) irq_handler1);
+    NVIC_EnableIRQ(Interrupt1_IRQn);
+    NVIC_SetPendingIRQ(Interrupt1_IRQn);
 
-	NVIC_SetVector(Interrupt79_IRQn, (uintptr_t)irq_handler_gpio);
-	NVIC_EnableIRQ(Interrupt79_IRQn);
+    NVIC_SetVector(Interrupt79_IRQn, (uintptr_t) irq_handler_gpio);
+    NVIC_EnableIRQ(Interrupt79_IRQn);
 
-	hardware_init_hook();
+    hardware_init_hook();
 
-	pm_register_ops(PM_DEV);
-	pm_register_ops(PM_NOIRQ_DEV);
-//
-	void task1(void* arg);
-	void task2(void* arg);
+    pm_register_ops(PM_DEV);
+    pm_register_ops(PM_NOIRQ_DEV);
+    //
+    void task1(void *arg);
+    void task2(void *arg);
 
-	// clear_ddr();
+    // clear_ddr();
 
-	pin_set_iomux(KEY_EINT_PIN_GROUP, KEY_EINT_PIN, 3);
-	pin_set_iomux(KEY_EINT_PIN_GROUP, 11, 3);
+    pin_set_iomux(KEY_EINT_PIN_GROUP, KEY_EINT_PIN, 3);
+    pin_set_iomux(KEY_EINT_PIN_GROUP, 11, 3);
 
-	gpio_init_config_t gpio_init_config = {
-	    .group = KEY_EINT_PIN_GROUP,
-	    .pin = KEY_EINT_PIN,
-	    .gpio_control_mode = Software_Mode,
-		.gpio_mode = GPIO_Falling_Int_Mode,
-		// .gpio_mode = GPIO_Input_Mode,
-	};
-	gpio_init(&gpio_init_config);
+    gpio_init_config_t gpio_init_config = {
+        .group             = KEY_EINT_PIN_GROUP,
+        .pin               = KEY_EINT_PIN,
+        .gpio_control_mode = Software_Mode,
+        .gpio_mode         = GPIO_Falling_Int_Mode,
+        // .gpio_mode = GPIO_Input_Mode,
+    };
+    gpio_init(&gpio_init_config);
 
-	gpio_init_config.pin = 11;
-	gpio_init(&gpio_init_config);
+    gpio_init_config.pin = 11;
+    gpio_init(&gpio_init_config);
 
-	__nouse__ u32 val = 0;
-	__nouse__ u32 count = 0;
+    __nouse__ u32 val   = 0;
+    __nouse__ u32 count = 0;
 #if 0
 	struct pmic_cfg cfg;
 	strcpy(cfg.name, "axp2101");
@@ -250,7 +240,7 @@ int main()
 	axp2101_powerkey_suspend();
 	// axp2101_powerkey_resume();
 #endif
-
+#if 0
 	while(1){
 #if 0
 		systimer_delay(1, IN_S);
@@ -321,11 +311,17 @@ int main()
 		printf("IP[%d] = 0x%x\n", Interrupt79_IRQn, val);
 		systimer_delay(2, IN_S);
 	}
-	// if (xTaskCreateStatic(task1, "task1", STACK_SIZE, NULL, 1, xStack, &xTaskBuffer) == NULL)
-		// while (1);
+#endif
 
-	// if (xTaskCreate(task2, "task2", 512, NULL, 1, NULL) != pdPASS)
-		// while (1);
-	// vTaskStartScheduler();
-	return 0;
+    if (xTaskCreateStatic(task1, "task1", STACK_SIZE, NULL, 1, xStack,
+                          &xTaskBuffer) == NULL)
+        while (1)
+            ;
+
+    if (xTaskCreate(task2, "task2", 512, NULL, 1, NULL) != pdPASS)
+        while (1)
+            ;
+    vTaskStartScheduler();
+
+    return 0;
 }
