7ec5a82deb63e3628fcecdda43bd432511e8f9a8
sk <astronautsk@qq.com>
2022-10-14 10:32:21 +0800

    [gic] - 4core触发SPI正常.

--- app/a55/dw_apb_timer/src/dw_apb_timer_test.c
+++ app/a55/dw_apb_timer/src/dw_apb_timer_test.c
@@ -31,7 +31,7 @@ static volatile uint32_t timerx6_t5_i_copy = 0;
 static volatile uint32_t timerx6_t6_i_copy = 0;
 void dw_apb_timer_test(bool sample)
 {
-	GIC_Enable();
+	// GIC_Enable();
 #ifdef SIMPALE_TEST
 	printf("dw_apb_timer_test 1.\n\r");
 	uint32_t tick = 0;
@@ -83,19 +83,19 @@ void dw_apb_timer_test(bool sample)
 	IRQ_Enable(Timerx2_T1_IRQn);
 
 	void timerx2_t2_irqhandler(void);
-	GIC_SetTarget(Timerx2_T2_IRQn, 1 << 0);
+	GIC_SetTarget(Timerx2_T2_IRQn, 1 << 1);
 	IRQ_SetHandler(Timerx2_T2_IRQn, timerx2_t2_irqhandler);
 	IRQ_SetPriority(Timerx2_T2_IRQn, 0 << 3);
 	IRQ_Enable(Timerx2_T2_IRQn);
 
 	void timerx6_t1_irqhandler(void);
-	GIC_SetTarget(Timerx6_T1_IRQn, 1 << 0);
+	GIC_SetTarget(Timerx6_T1_IRQn, 1 << 2);
 	IRQ_SetHandler(Timerx6_T1_IRQn, timerx6_t1_irqhandler);
 	IRQ_SetPriority(Timerx6_T1_IRQn, 0 << 3);
 	IRQ_Enable(Timerx6_T1_IRQn);
 
 	void timerx6_t2_irqhandler(void);
-	GIC_SetTarget(Timerx6_T2_IRQn, 1 << 0);
+	GIC_SetTarget(Timerx6_T2_IRQn, 1 << 3);
 	IRQ_SetHandler(Timerx6_T2_IRQn, timerx6_t2_irqhandler);
 	IRQ_SetPriority(Timerx6_T2_IRQn, 0 << 3);
 	IRQ_Enable(Timerx6_T2_IRQn);
@@ -107,19 +107,19 @@ void dw_apb_timer_test(bool sample)
 	IRQ_Enable(Timerx6_T3_IRQn);
 
 	void timerx6_t4_irqhandler(void);
-	GIC_SetTarget(Timerx6_T4_IRQn, 1 << 0);
+	GIC_SetTarget(Timerx6_T4_IRQn, 1 << 1);
 	IRQ_SetHandler(Timerx6_T4_IRQn, timerx6_t4_irqhandler);
 	IRQ_SetPriority(Timerx6_T4_IRQn, 0 << 3);
 	IRQ_Enable(Timerx6_T4_IRQn);
 
 	void timerx6_t5_irqhandler(void);
-	GIC_SetTarget(Timerx6_T5_IRQn, 1 << 0);
+	GIC_SetTarget(Timerx6_T5_IRQn, 1 << 2);
 	IRQ_SetHandler(Timerx6_T5_IRQn, timerx6_t5_irqhandler);
 	IRQ_SetPriority(Timerx6_T5_IRQn, 0 << 3);
 	IRQ_Enable(Timerx6_T5_IRQn);
 
 	void timerx6_t6_irqhandler(void);
-	GIC_SetTarget(Timerx6_T6_IRQn, 1 << 0);
+	GIC_SetTarget(Timerx6_T6_IRQn, 1 << 3);
 	IRQ_SetHandler(Timerx6_T6_IRQn, timerx6_t6_irqhandler);
 	IRQ_SetPriority(Timerx6_T6_IRQn, 0 << 3);
 	IRQ_Enable(Timerx6_T6_IRQn);
--- app/a55/dw_apb_timer/src/main.c
+++ app/a55/dw_apb_timer/src/main.c
@@ -1,14 +1,53 @@
-#ifndef QEMU
+// #ifndef QEMU
 #include <stdio.h>
 #include <stdbool.h>
+#include <stdint.h>
+#include <wakeup_core.h>
+#include <assert.h>
+#include <systimer.h>
+
+uint64_t* secondary_cores_entry1 = (void*)0x0000000000200410;
+
+static void wakeup_core1(uint8_t core_id, void* func)
+{
+	assert((core_id<=3) && (core_id!=0));
+	if(core_id==get_core_id())	return;
+	secondary_cores_entry1[(core_id-1)*2+0] = (uint64_t)func;
+	secondary_cores_entry1[(core_id-1)*2+1] = 0x123456788654321;
+	asm volatile("dsb 0xf":::"memory");
+	asm volatile("sev");
+	return;
+}
+
+void corex_c_entry(void)
+{
+	GIC_CPUInterfaceInit(); //per CPU
+	printf("hello world in core.%d\n\r", get_core_id());
+	while(1);
+}
+
 
 int main()
 {
+	printf("hello world in core.%d\n\r", get_core_id());
+	systimer_init();
+	GIC_DistInit();
+  	GIC_CPUInterfaceInit(); //per CPU
+	extern void entrypoint();
+	wakeup_core1(1, entrypoint);
+	wakeup_core1(2, entrypoint);
+	wakeup_core1(3, entrypoint);
+
+	wakeup_core(1, corex_c_entry);
+	wakeup_core(2, corex_c_entry);
+	wakeup_core(3, corex_c_entry);
+	systimer_delay(1, IN_S);
 	void dw_apb_timer_test(bool sample);
-	dw_apb_timer_test(true);
+	dw_apb_timer_test(false);
+	while(1);
 	return 0;
 }
 
-#else
-#include "main_qemu.c"
-#endif
+// #else
+// #include "main_qemu.c"
+// #endif
