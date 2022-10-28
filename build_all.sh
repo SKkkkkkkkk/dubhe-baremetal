005e778984305907287e60e2e803620865594f52
sk <1193375474@qq.com>
2022-10-28 16:32:25 +0800

    add build_all.sh.

---
+++ build_all.sh
@@ -0,0 +1,62 @@
+cd app/a55/cli/
+./build.sh
+
+cd -
+cd app/a55/dw_apb_ssi
+./build.sh
+
+cd -
+cd app/a55/dw_apb_ssi
+./build.sh
+
+cd -
+cd app/a55/dw_apb_timer
+./build.sh
+
+cd - 
+cd app/a55/fwu_dram
+./build.sh
+
+cd - 
+cd app/a55/fwu_sram
+./build.sh
+
+cd - 
+cd app/a55/generic_timers
+./build.sh
+
+cd - 
+cd app/a55/gpio
+./build.sh
+
+cd - 
+cd app/a55/hello
+./build.sh
+
+cd - 
+cd app/a55/i2c
+./build.sh
+
+cd - 
+cd app/cv32e40p/hello
+./build.sh
+
+cd - 
+cd app/m3/cli
+./build.sh
+
+cd - 
+cd app/m3/dw_apb_timer
+./build.sh
+
+cd - 
+cd app/m3/freertos_basic
+./build.sh
+
+cd - 
+cd app/m3/pm_demo
+./build.sh
+
+cd - 
+cd app/m3/rom2xxx
+./build.sh
