39dea8e09e8015b08a04368fc781ee5873546414
sk <1193375474@qq.com>
2022-10-28 18:06:12 +0800

    pinmux

---
+++ hw/pinmux/inc/pinmux.h
@@ -0,0 +1,17 @@
+#ifndef __APE1210_TIMER_H__
+#define __APE1210_TIMER_H__
+
+#include <stdint.h>
+#include <stdbool.h>
+
+#ifdef __cplusplus
+    extern "C" {
+#endif
+
+int pinmux(uint8_t gpio, uint8_t);
+
+#ifdef __cplusplus
+    }
+#endif
+
+#endif
