35c5774e22d4ca352847fa2f8b3027652a58e1d5
sk <1193375474@qq.com>
2022-11-04 16:25:54 +0800

    [wdt] - wdt pass, 24Mhz.

---
+++ hw/wdt/dw_apb_wdt/src/dw_apb_wdt.c
@@ -0,0 +1,38 @@
+#ifdef A55
+	#include <ca55_chip_define.h>
+#else
+	#include <cm3_chip_define.h>
+#endif
+
+#include <regs_type.h>
+#include "dw_apb_wdt_regs.h"
+
+#define WDT ((DW_APB_WDT_TypeDef *)WDT0_BASE)
+
+/*
+0--ffff
+1--1_FFFF
+2--3_FFFF
+...
+f--7FFF_FFFF
+*/
+
+void wdt_start(uint8_t time)
+{
+	if (WDT->CR & WDT_EN_Msk)
+		return;
+	WDT->TORR = time;
+	WDT->CRR = 0x76UL;
+
+	WDT->CR &= ~RMOD_MsK;
+	WDT->CR |= WDT_EN_Msk;
+}
+
+
+/**
+ * @brief 喂狗
+ */
+void wdt_feed(void)
+{
+	WDT->CRR = 0x76UL;
+}
