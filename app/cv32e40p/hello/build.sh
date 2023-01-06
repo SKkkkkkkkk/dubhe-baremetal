099d1daf88c611747fc7946ccbaca1c37c0d4ea9
sk <1193375474@qq.com>
2023-01-06 16:19:08 +0800

    -DBOARD=FPGA <= -DBOARD=RTL.

--- app/cv32e40p/hello/build.sh
+++ app/cv32e40p/hello/build.sh
@@ -1,2 +1,2 @@
-CROSS_COMPILE=riscv64-unknown-elf- cmake -S ../../../ -B build -DPROJECT_NAME=hello -DPROJECT_PATH=. -DCORE=CV32E40P -DBOARD=RTL -DBUILD_TYPE=Debug
+CROSS_COMPILE=riscv64-unknown-elf- cmake -S ../../../ -B build -DPROJECT_NAME=hello -DPROJECT_PATH=. -DCORE=CV32E40P -DBOARD=FPGA -DBUILD_TYPE=Debug
 cmake --build build
--- app/m3/dw_apb_timer/build.sh
+++ app/m3/dw_apb_timer/build.sh
@@ -1,2 +1,2 @@
-CROSS_COMPILE=arm-none-eabi- cmake -S ../../../ -B build -DPROJECT_NAME=dw_apb_timer -DPROJECT_PATH=. -DCORE=M3 -DBOARD=RTL -DBUILD_TYPE=Debug -DWHERE_TO_LOAD=ALL_IN_RAM
+CROSS_COMPILE=arm-none-eabi- cmake -S ../../../ -B build -DPROJECT_NAME=dw_apb_timer -DPROJECT_PATH=. -DCORE=M3 -DBOARD=FPGA -DBUILD_TYPE=Debug -DWHERE_TO_LOAD=ALL_IN_RAM
 cmake --build build
