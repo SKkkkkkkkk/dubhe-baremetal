a5dfef3c1cd0757ff4c75c8ffc766623313c6519
sk <1193375474@qq.com>
2023-01-10 15:37:24 +0800

    [M3] - reduce __HEAP_SIZE.

--- hw/cores/arm/armv7/m/coretex-m3/inc/mem_and_clock.h
+++ hw/cores/arm/armv7/m/coretex-m3/inc/mem_and_clock.h
@@ -23,7 +23,7 @@
 	#define __EXTERNAL_RAM_SIZE	0x00100000
 
 	#define __STACK_SIZE 0x00000400
-	#define __HEAP_SIZE  0x00004000 //???
+	#define __HEAP_SIZE  0x00000800
 #else
 	#error "unkowned board."
 #endif
