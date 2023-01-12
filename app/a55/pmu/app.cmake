9d41d4d5bc15ff57642b45e8c66b133a14f8e922
lizhenhui <lizhenhui@seehi.com>
2023-01-12 19:53:11 +0800

    [pmu] add pmic rtc
    Signed-off-by: lizhenhui <lizhenhui@seehi.com>

--- app/a55/pmu/app.cmake
+++ app/a55/pmu/app.cmake
@@ -5,8 +5,9 @@ set(app_src
 	# ${CMAKE_CURRENT_LIST_DIR}/src/pmu_dyn_pwr_a55.c
 	# ${CMAKE_CURRENT_LIST_DIR}/src/pmu_sleep_wakeup_a55.c
 	# ${CMAKE_CURRENT_LIST_DIR}/src/pmu_sleep_wakeup_a55_core_ap.c
-	${CMAKE_CURRENT_LIST_DIR}/src/pmu_sleep_wakeup_pmic.c
+	# ${CMAKE_CURRENT_LIST_DIR}/src/pmu_sleep_wakeup_pmic.c
 	# ${CMAKE_CURRENT_LIST_DIR}/src/pmu_dyn_pwr_a55_m3.c
+	${CMAKE_CURRENT_LIST_DIR}/src/pmu_sleep_wakeup_pmic_rtc.c
 )
 
 # 添加app include dirs
