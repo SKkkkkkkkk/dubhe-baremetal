b2bfe1846315e51b0351e7d20dddc80846589fbf
sk <1193375474@qq.com>
2023-02-20 20:32:47 +0800

    fix secondary cores can not be wakeup.

--- app/a55/hello/src/main_qemu.c
+++ app/a55/hello/src/main_qemu.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "wakeup_core.h"
 
 #ifdef __GNUC__
 	#pragma GCC diagnostic push
@@ -7,11 +8,20 @@
 	#pragma GCC diagnostic ignored "-Wunused-but-set-variable"
 #endif
 
+void secondary_entry(void)
+{
+	printf("this is core.%d\n\r", get_core_id());
+	while(1);
+}
+
 int main()
 {
 	printf("M2v5: "BUILD_TIMESTAMP"\n\r");
 	printf("git hash: "GIT_HASH"\n\r");
 	printf("A55: hello world.\n\r");
+	wakeup_core(1, secondary_entry);
+	wakeup_core(2, secondary_entry);
+	wakeup_core(3, secondary_entry);
 	return 0;
 }
 
