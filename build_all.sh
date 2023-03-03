9336826b561226159220cf9ec702f12a9e76ab63
sk <1193375474@qq.com>
2023-03-03 15:16:16 +0800

    move section .ram to the end of output sections.

--- build_all.sh
+++ build_all.sh
@@ -3,86 +3,81 @@ rm -rf build;
 ./build.sh;
 
 cd - ;
+cd app/a55/dw_apb_ssi;
 rm -rf build;
-cd app/a55/dw_apb_ssi;
 ./build.sh;
 
 cd - ;
-rm -rf build ;
-cd app/a55/dw_apb_ssi;
-./build.sh;
-
-cd - ;
-rm -rf build ;
 cd app/a55/dw_apb_timer;
+rm -rf build;
 ./build.sh;
 
 cd - ;
-rm -rf build ;
 cd app/a55/dw_mbox_a2b;
+rm -rf build;
 ./build.sh;
 
 cd - ;
-rm -rf build ;
 cd app/a55/freertos_basic;
+rm -rf build;
 ./build.sh;
 
 cd - ;
-rm -rf build  ;
 cd app/a55/fwu_dram;
+rm -rf build;
 ./build.sh;
 
 cd - ;
-rm -rf build ;
 cd app/a55/fwu_sram;
+rm -rf build;
 ./build.sh;
 
 cd - ;
-rm -rf build ;
 cd app/a55/generic_timers;
+rm -rf build;
 ./build.sh;
 
 cd - ;
-rm -rf build ;
 cd app/a55/gpio;
+rm -rf build;
 ./build.sh;
 
 cd - ;
-rm -rf build ;
 cd app/a55/hello;
+rm -rf build;
 ./build.sh;
 
 cd - ;
-rm -rf build ;
 cd app/a55/i2c;
+rm -rf build;
 ./build.sh;
 
 cd - ;
-rm -rf build ;
 cd app/cv32e40p/hello;
+rm -rf build;
 ./build.sh;
 
 cd - ;
-rm -rf build ;
 cd app/m3/cli;
+rm -rf build;
 ./build.sh;
 
 cd - ;
-rm -rf build ;
 cd app/m3/dw_apb_timer;
+rm -rf build;
 ./build.sh;
 
 cd - ;
-rm -rf build ;
 cd app/m3/freertos_basic;
+rm -rf build;
 ./build.sh;
 
 cd -;
-rm -rf build;
 cd app/m3/pm_demo;
+rm -rf build;
 ./build.sh;
 
 cd -;
-rm -rf build;
 cd app/m3/rom2xxx;
+rm -rf build;
 ./build.sh;
