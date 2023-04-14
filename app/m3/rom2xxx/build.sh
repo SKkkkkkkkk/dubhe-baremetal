d1de9434848b166af99fdf5f3bbba2314fd0d1a8
sk <1193375474@qq.com>
2023-04-14 10:57:15 +0800

    M3 now can only use the last 8KB SYSRAM. 1. .ram section has been moved from SYSRAM to AXI_SRAM.

--- app/m3/rom2xxx/build.sh
+++ app/m3/rom2xxx/build.sh
@@ -1,2 +1,2 @@
-CROSS_COMPILE=arm-none-eabi- cmake -S ../../../ -B build -DPROJECT_NAME=rom2xxx -DPROJECT_PATH=. -DCORE=M3 -DBOARD=FPGA -DBUILD_TYPE=Debug -DWHERE_TO_LOAD=ALL_IN_SYSRAM
+CROSS_COMPILE=arm-none-eabi- cmake -S ../../../ -B build -DPROJECT_NAME=rom2xxx -DPROJECT_PATH=. -DCORE=M3 -DBOARD=FPGA -DBUILD_TYPE=Debug -DWHERE_TO_LOAD=ROM
 cmake --build build -j100
--- hw/cores/arm/armv7/m/coretex-m3/inc/mem_and_clock.h
+++ hw/cores/arm/armv7/m/coretex-m3/inc/mem_and_clock.h
@@ -14,12 +14,12 @@
 #elif  defined(RTL) ||  defined(FPGA) ||  defined(EVB) // RTL, FPGA, EVB
 	#define __CODE_BASE 		0x00000000
 	#define __AXI_SRAM_BASE		0x00100000
-	#define __SRAM_BASE			0x20038000 //最后32KB
+	#define __SRAM_BASE			0x2003E000 //最后8KB
 	#define __EXTERNAL_RAM_BASE	0x60000000
 
 	#define __CODE_SIZE 		0x00010000
 	#define __AXI_SRAM_SIZE		0x00008000
-	#define __SRAM_SIZE 		0x00008000 //最后32KB
+	#define __SRAM_SIZE 		0x00002000 //最后8KB
 	#define __EXTERNAL_RAM_SIZE	0x00100000
 
 	#define __STACK_SIZE 0x00000400
