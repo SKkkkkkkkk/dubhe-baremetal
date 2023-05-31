1d35e567753f275bbaf846835b9f3878af272de8
sk <1193375474@qq.com>
2023-05-31 16:06:37 +0800

    build_all.sh

--- build_all.sh
+++ build_all.sh
@@ -1,83 +1,67 @@
-cd app/a55/cli/;
+(cd app/a55/cli/;
 rm -rf build;
-./build.sh;
+./build.sh;)
 
-cd - ;
-cd app/a55/dw_apb_ssi;
+(cd app/a55/dw_apb_ssi;
 rm -rf build;
-./build.sh;
+./build.sh;)
 
-cd - ;
-cd app/a55/dw_apb_timer;
+(cd app/a55/dw_apb_timer;
 rm -rf build;
-./build.sh;
+./build.sh;)
 
-cd - ;
-cd app/a55/dw_mbox_a2b;
+(cd app/a55/dw_mbox_a2b;
 rm -rf build;
-./build.sh;
+./build.sh;)
 
-cd - ;
-cd app/a55/freertos_basic;
+(cd app/a55/freertos_basic;
 rm -rf build;
-./build.sh;
+./build.sh;)
 
-cd - ;
-cd app/a55/fwu_dram;
+(cd app/a55/fwu_dram;
 rm -rf build;
-./build.sh;
+./build.sh;)
 
-cd - ;
-cd app/a55/fwu_sram;
+(cd app/a55/fwu_sram;
 rm -rf build;
-./build.sh;
+./build.sh;)
 
-cd - ;
-cd app/a55/generic_timers;
+(cd app/a55/generic_timers;
 rm -rf build;
-./build.sh;
+./build.sh;)
 
-cd - ;
-cd app/a55/gpio;
+(cd app/a55/gpio;
 rm -rf build;
-./build.sh;
+./build.sh;)
 
-cd - ;
-cd app/a55/hello;
+(cd app/a55/hello;
 rm -rf build;
-./build.sh;
+./build.sh;)
 
-cd - ;
-cd app/a55/i2c;
+(cd app/a55/i2c;
 rm -rf build;
-./build.sh;
+./build.sh;)
 
-cd - ;
-cd app/cv32e40p/hello;
+(cd app/cv32e40p/hello;
 rm -rf build;
-./build.sh;
+./build.sh;)
 
-cd - ;
-cd app/m3/cli;
+(cd app/m3/cli;
 rm -rf build;
-./build.sh;
+./build.sh;)
 
-cd - ;
-cd app/m3/dw_apb_timer;
+(cd app/m3/dw_apb_timer;
 rm -rf build;
-./build.sh;
+./build.sh;)
 
-cd - ;
-cd app/m3/freertos_basic;
+(cd app/m3/freertos_basic;
 rm -rf build;
-./build.sh;
+./build.sh;)
 
-cd -;
-cd app/m3/pm_demo;
+(cd app/m3/pm_demo;
 rm -rf build;
-./build.sh;
+./build.sh;)
 
-cd -;
-cd app/m3/rom2xxx;
+(cd app/m3/rom2xxx;
 rm -rf build;
-./build.sh;
+./build.sh;)
