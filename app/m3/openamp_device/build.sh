7256730396e2ae58f33be036a20d6529d79e5540
lizhenhui <lizhenhui@seehi.com>
2023-06-20 16:34:45 +0800

    [openamp] seem m3 openamp ok
    Signed-off-by: lizhenhui <lizhenhui@seehi.com>

--- app/m3/openamp_device/build.sh
+++ app/m3/openamp_device/build.sh
@@ -1,2 +1,2 @@
-CROSS_COMPILE=arm-none-eabi- cmake -S ../../../ -B build -DPROJECT_NAME=openamp_device -DPROJECT_PATH=. -DCORE=M3 -DBOARD=QEMU -DBUILD_TYPE=Debug -DWHERE_TO_LOAD=ROM
+CROSS_COMPILE=arm-none-eabi- cmake -S ../../../ -B build -DPROJECT_NAME=openamp_device -DPROJECT_PATH=. -DCORE=M3 -DBOARD=FPGA -DBUILD_TYPE=Debug -DWHERE_TO_LOAD=ALL_IN_DDR
 cmake --build build -j100
